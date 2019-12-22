f4d10609d6d94b82e74dbf6a94ca9944b1a8708c
joaossmacedo <joaossmacedo@gmail.com>
2019-12-21 22:41:51 -0300

    add simple validations

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputComponent } from './input/input.component';
+import { IonicModule } from '@ionic/angular';
 
 
 
 @NgModule({
   declarations: [InputComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule
   ],
   exports: [InputComponent]
 })
--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-input',
@@ -6,9 +6,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input.component.scss'],
 })
 export class InputComponent implements OnInit {
+  @Input() type;
+  @Output() user_input = new EventEmitter();
+
+  errors;
+
+  value;
+
+  ionViewWillEnter() {
+    this.value = '';
+  }
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.errors = [];
+    // const error = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
+    //   ' Mauris vulputate nec erat in dignissim. Donec ut ligula' +
+    //   ' ac risus dignissim.';
+    // this.errors.push(error);
+  }
 
+  hasError(){
+    return this.errors && this.errors.length > 0
+  }
+
+  validate(value) {
+    console.log('validating ' + this.type)
+    console.log('value: ' + value)
+    var errors = []
+
+    if (this.type === 'email') {
+      errors = this.validateEmail(value);
+    } else if (this.type === 'password') {
+      errors = this.validatePassword(value);
+    }
+
+    this.errors = errors;
+  }
+
+
+  validateEmail(email) {
+    var ret = [];
+    
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    
+    if(!re.test(String(email).toLowerCase())){
+      ret.push('invalid email')
+    }
+
+    return ret;
+  }
+
+  validatePassword(password) {
+    var ret = [];
+    
+    const re = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$/
+    
+    if(!re.test(String(password))){
+      ret.push('password must contain at least 6 characters, 1 special character, 1 number, 1 uppercase and 1 lowercase')
+    }
+
+    return ret;
+  }
 }
