d265ca4a2c68aa87a98d1d56e609d15e6548f613
joaossmacedo <joaossmacedo@gmail.com>
2020-02-07 22:55:48 -0300

    [FEATURE] - Add services for mask and validation

---
+++ src/app/services/mask.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MaskService {
+
+  constructor() { }
+
+  maskPhone(event) {
+    // I'm using the brazilian format for phones "(00)0000-0000" or "(00)00000-0000".
+    // We decide if we should use the 9 or 8 digit format based on its value length.
+    let value: string = event.value;
+
+    if (value.length > 14) {
+      value = value.substr(0, value.length - 1);
+    }
+
+    const len = value.length;
+    if (len === 1 && value[len - 1] === '(') {
+      value = '';
+    } else if (len === 1) {
+      value = '(' + value;
+    } else if (len === 4  && value[len - 1] === ')') {
+      value = value.substr(0, len - 1);
+    } else if (len === 4) {
+      value = value.substr(0, len - 1) + ')' + value[len - 1];
+    } else if (len === 9  && value[len - 1] === '-') {
+      value = value.substr(0, len - 1);
+    } else if (len === 9) {
+      value = value.substr(0, len - 1) + '-' + value[len - 1];
+    } else if (len === 13  && value[9] === '-') {
+      value = value.replace('-', '');
+      value = value.substr(0, 8) + '-' + value.substr(8, len - 1);
+    } else if (len === 14) {
+      value = value.replace('-', '');
+      value = value.substr(0, 9) + '-' + value.substr(9, len - 1);
+    }
+
+    event.value = value;
+  }
+}
---
+++ src/app/services/validation.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ValidationService {
+
+  constructor() { }
+
+  validatePhone(input) {
+    return [];
+  }
+
+  validateEmail(email) {
+    const ret = [];
+
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (!re.test(String(email).toLowerCase())) {
+      ret.push('invalid email')
+    }
+
+    return ret;
+  }
+
+  validatePassword(password) {
+    const ret = [];
+
+    const re = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$/;
+
+    if (!re.test(String(password))) {
+      ret.push('password must contain at least 6 characters, 1 special character, 1 number, 1 uppercase and 1 lowercase');
+    }
+
+    return ret;
+  }
+}
