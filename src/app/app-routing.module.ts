36bbb2125c4c868d535d4a0d7472301632a12ca4
joaossmacedo <joaossmacedo@gmail.com>
2020-02-09 22:28:39 -0300

    [BUGFIX] - Removes side menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,16 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'form',
     pathMatch: 'full'
-  },
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
-  },
-  {
-    path: 'list',
-    loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
   },
   {
     path: 'form',
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,23 +10,6 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  public appPages = [
-    {
-      title: 'Home',
-      url: '/home',
-      icon: 'home'
-    },
-    {
-      title: 'List',
-      url: '/list',
-      icon: 'list'
-    },
-    {
-      title: 'Form',
-      url: '/form',
-      icon: 'chatboxes' 
-    }
-  ];
 
   constructor(
     private platform: Platform,
